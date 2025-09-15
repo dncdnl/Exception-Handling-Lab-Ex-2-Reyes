0e88808005dab1c04687de5d610059cc25121b6a
dncdnl <danicadaniela.cesicar@gmail.com>
2025-09-15 20:11:02 +0800

    Add project files.

---
+++ Exception-Handling-Lab-Ex-2-Reyes/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exception_Handling_Lab_Ex_2_Reyes
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
