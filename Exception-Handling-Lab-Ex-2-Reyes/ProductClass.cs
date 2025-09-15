8f0f922b369996fda24d7c7f1f5a3d8bbd227165
dncdnl <danicadaniela.cesicar@gmail.com>
2025-09-15 20:55:54 +0800

    Creating a Class called product class

---
+++ Exception-Handling-Lab-Ex-2-Reyes/ProductClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Lab_Ex_2_Reyes
+{
+    internal class ProductClass
+    {
+    }
+}
